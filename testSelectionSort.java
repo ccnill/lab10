50d97981e5b2deba14cbdcd4f4abfa512aa2da3f
Christian Nill <nill8nill@gmail.com>
2019-04-04 15:18:05 -0400

    mixed and duplicate

--- testSelectionSort.java
+++ testSelectionSort.java
@@ -15,10 +15,11 @@ public void test() {
 //testPositive();
 //testNegative();
 testDuplicates();
-//testMixed();
+testMixed();
 }
     public testSelectionSort() {
     }
+    /*
     public void testPositive(){
         int[] arr = new int[5];
         arr[0] = 8;
@@ -34,13 +35,11 @@ testDuplicates();
         Sortedarr[2] = 8;
         Sortedarr[3] = 9;
         Sortedarr[4] = 10;
-        /** add tests to check for this unit test **/
         for(int i = 0; i < 5; i++) {
         	assertEquals(arr[i], Sortedarr[i]);
         }
     }
     public void testNegative(){
-        /** Test data contains negative values only **/
     	int[] arr = new int[5];
         arr[0] = -8;
         arr[1] = -9;
@@ -55,11 +54,11 @@ testDuplicates();
         Sortedarr[2] = -8;
         Sortedarr[3] = -7;
         Sortedarr[4] = -2;
-        /** add tests to check for this unit test **/
         for(int i = 0; i < 5; i++) {
         	assertEquals(arr[i], Sortedarr[i]);
         }
     }
+    */
     public void testMixed(){
         /** Test data contains with both positive, negative and zeros **/
     	int[] arr = new int[5];
@@ -102,4 +101,4 @@ testDuplicates();
         	assertEquals(arr[i], Sortedarr[i]);
         }
     }
-}	
+}
